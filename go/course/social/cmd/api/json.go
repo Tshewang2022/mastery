bacbd205b9708cd3cefd4c58daa34247a38f514b
Tshewang2022 <tshewanggyaltshen0@gmail.com>
2026-01-05 22:38:29 -0600

    updated the code

---
+++ go/course/social/cmd/api/json.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func writeJSON(w http.ResponseWriter, status int, data any) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	return json.NewEncoder(w).Encode(data)
+}
+
+func readJSON(w http.ResponseWriter, r *http.Request, data any) error {
+	// block the size of the requrest
+	maxBytes := 1_048_578
+
+	decoder := json.NewDecoder(r.Body)
+	return decoder.Decode(data)
+}
