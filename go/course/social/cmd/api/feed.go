3cbf3af2c2eb61b47015e2b0bf8af79ac58a547a
Tshewang2022 <tshewanggyaltshen0@gmail.com>
2026-01-10 01:56:46 -0600

    done with the user feed algo

--- go/course/social/cmd/api/feed.go
+++ go/course/social/cmd/api/feed.go
@@ -6,13 +6,13 @@ import "net/http"
 func (app *application) getUserFeedHandler(w http.ResponseWriter, r *http.Request) {
 	// TODO: pagination, filters
 	ctx := r.Context()
-	feed, err := app.store.Posts.GetUserFeed(ctx, int64(42))
+	feed, err := app.store.Posts.GetUserFeed(ctx, int64(103))
 
 	if err != nil {
 		app.internalServerError(w, r, err)
 	}
 
-	if err := app.jsonResponse(w, r, feed); err != nil {
+	if err := app.jsonResponse(w, http.StatusOK, feed); err != nil {
 		app.internalServerError(w, r, err)
 	}
 }
