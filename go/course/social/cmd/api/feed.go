41d6e5c62685dc27a53abc4fb75f26044b67ec28
Tshewang2022 <tshewanggyaltshen0@gmail.com>
2026-01-10 02:36:10 -0600

    updated the code but some issue there

--- go/course/social/cmd/api/feed.go
+++ go/course/social/cmd/api/feed.go
@@ -1,12 +1,33 @@
 package main
 
-import "net/http"
+import (
+	"github/Tshewang2022/social/internal/store"
+	"net/http"
+)
 
 // func getUserFeedHandler(req, res)=>{}
 func (app *application) getUserFeedHandler(w http.ResponseWriter, r *http.Request) {
 	// TODO: pagination, filters
+	fq := store.PaginatedFeedQuery{
+		Limit:  20,
+		Offset: 0,
+		Sort:   "desc",
+	}
+
+	fq, err := fq.Parse(r)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	if err := Validate.Struct(fq); err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
 	ctx := r.Context()
-	feed, err := app.store.Posts.GetUserFeed(ctx, int64(103))
+
+	feed, err := app.store.Posts.GetUserFeed(ctx, int64(103), fq)
 
 	if err != nil {
 		app.internalServerError(w, r, err)
